3ec7f103973dd58300fd9abbbe4e19ad024fdc7e
monjara <yaginumee@gmail.com>
2022-06-25 11:09:31 +0900

    add setting

--- nvim/init.lua
+++ nvim/init.lua
@@ -134,6 +134,7 @@ require('packer').startup(function(use)
       vim.keymap.set('n', '<space>fb', "<cmd>lua  require('fzf-lua').buffers()<CR>", opts)
       vim.keymap.set('n', '<space>fm', "<cmd>lua  require('fzf-lua').marks()<CR>", opts)
       vim.keymap.set('n', '<space>fr', "<cmd>lua  require('fzf-lua').grep()<CR>", opts)
+      vim.keymap.set('n', '<space>fp', "<cmd>lua  require('fzf-lua').registers()<CR>", opts)
       vim.keymap.set('n', '<space>gs', "<cmd>lua  require('fzf-lua').git_status()<CR>", opts)
       -- TODO add keymap
     end
