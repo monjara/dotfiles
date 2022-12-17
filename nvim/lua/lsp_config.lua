39d8e2949b2ed5785d495df6946920ae8ff0779b
monjara <yaginumee@gmail.com>
2022-12-17 21:51:33 +0900

    fix setting

--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -25,28 +25,54 @@ require('mason-lspconfig').setup({
 local lspconfig = require('lspconfig')
 require('mason-lspconfig').setup_handlers {
 
-
   function(server_name)
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
     local lsp_flags = {
       debounce_text_changes = 150,
     }
+    local on_attach = function(_, bufnr)
+      vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+      local bufopts = { noremap = true, silent = true, buffer = bufnr }
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+      vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+      vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+      vim.keymap.set('n', '<space>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end, bufopts)
+      vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+      vim.keymap.set('n', '<Space>rn', vim.lsp.buf.rename, bufopts)
+      vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+      vim.keymap.set('n', '<leader>fo', vim.lsp.buf.format, bufopts)
+    end
 
     lspconfig[server_name].setup {
       capabilities = capabilities,
-      lsp_flags = lsp_flags
+      lsp_flags = lsp_flags,
+      on_attach = on_attach
     }
   end,
 
   ['rust_analyzer'] = function()
-    local rust_tools = require('rust-tools')
-    rust_tools.setup {
+    local rt = require('rust-tools')
+    rt.setup {
         server = {
           on_attach = function(_, bufnr)
-            vim.keymap.set('n', '<C-space>', rust_tools.hover_actions.hover_actions, { buffer = bufnr })
-            vim.keymap.set('n', '<Leader>a', rust_tools.code_action_group.code_action_group, { buffer = bufnr })
+            vim.keymap.set('n', '<C-space>', rt.hover_actions.hover_actions, { buffer = bufnr })
+            vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
           end,
         },
+        dap = {
+          adapter = {
+            type = "executable",
+            command = ""
+          }
+        }
     }
   end,
 
