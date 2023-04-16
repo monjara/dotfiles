2385817116699d24c25b9b17abcbab824c3f6a76
monjara <yaginumee@gmail.com>
2023-04-16 23:46:05 +0900

    migrate to lua

--- nvim/lua/custom/help.lua
+++ nvim/lua/custom/help.lua
@@ -1,37 +1,34 @@
-vim.cmd([[
-function! CreateCenteredFloatingWindow() abort
-    let width = min([&columns - 4, max([80, &columns - 20])])
-    let height = min([&lines - 4, max([20, &lines - 10])])
-    let top = ((&lines - height) / 2) - 1
-    let left = (&columns - width) / 2
-    let opts = {'relative': 'editor', 'row': top, 'col': left, 'width': width, 'height': height, 'style': 'minimal'}
+local function createFloatingHelp(q)
+  local lines = vim.o.lines - vim.o.cmdheight
+  local columns = vim.o.columns
+  local height = lines - 4
+  local width = math.min(columns - 10, 80)
+  local row = math.floor((lines - height) / 2)
+  local col = math.floor((columns - width) / 2)
 
-    let top = "╭" . repeat("─", width - 2) . "╮"
-    let mid = "│" . repeat(" ", width - 2) . "│"
-    let bot = "╰" . repeat("─", width - 2) . "╯"
-    let lines = [top] + repeat([mid], height - 2) + [bot]
-    let s:buf = nvim_create_buf(v:false, v:true)
-    call nvim_buf_set_lines(s:buf, 0, -1, v:true, lines)
-    call nvim_open_win(s:buf, v:true, opts)
-    set winhl=Normal:Floating
-    let opts.row += 1
-    let opts.height -= 2
-    let opts.col += 2
-    let opts.width -= 4
-    let l:textbuf = nvim_create_buf(v:false, v:true)
-    call nvim_open_win(l:textbuf, v:true, opts)
-    au BufWipeout <buffer> exe 'bw '.s:buf
-    return l:textbuf
-endfunction
+  local opts = {
+    height = height,
+    width = width,
+    row = row,
+    col = col,
+    relative = 'editor',
+    style = 'minimal',
+    zindex = 45,
+  }
+  local bufnr = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_open_win(bufnr, true, opts)
+  vim.opt_local.filetype = 'help'
+  vim.opt_local.buftype = 'help'
+  vim.api.nvim_command('help ' .. q['args'])
+end
 
-function! FloatingWindowHelp(query) abort
-    let l:buf = CreateCenteredFloatingWindow()
-    call nvim_set_current_buf(l:buf)
-    setlocal filetype=help
-    setlocal buftype=help
-    execute 'help ' . a:query
-endfunction
-
-command! -complete=help -nargs=? H call FloatingWindowHelp(<q-args>)
-]])
-
+vim.api.nvim_create_user_command(
+  'H',
+  function(opts)
+    createFloatingHelp(opts)
+  end,
+  {
+    nargs = 1,
+    complete = 'help'
+  }
+)
