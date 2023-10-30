1d22de3b338bb8c41c54b8f0da6a5e4567803efe
monjara <yaginumee@gmail.com>
2023-10-30 10:14:42 +0900

    add shortcut

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -52,7 +52,9 @@ require('lazy').setup({
       require('gitsigns').setup()
     end,
     keys = {
-      { '<space>gd', '<cmd>Gitsigns diffthis<cr>', desc = 'gitsigns' }
+      { '<space>gd', '<cmd>Gitsigns diffthis<cr>', desc = 'gitsigns' },
+      { '<space>gb', '<cmd>Gitsigns blame_line<cr>', desc = 'gitsigns' },
+      { '<space>gtb', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = 'gitsigns' }
     },
   },
   {
