cb9456df11be743b7ee21fa2ca87e9a927fafa6e
monjara <yaginumee@gmail.com>
2023-10-31 15:40:17 +0900

    add noice config

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -31,7 +31,22 @@ require('lazy').setup({
     dependencies = {
       'MunifTanjim/nui.nvim',
       'rcarriga/nvim-notify',
-    }
+    },
+    config = function()
+      require('noice').setup({
+        lsp = {
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true,
+          },
+        },
+        presets = {
+          long_message_to_split = true, -- long messages will be sent to a split
+          lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
+      })
+    end
   },
   {
     'kylechui/nvim-surround',
