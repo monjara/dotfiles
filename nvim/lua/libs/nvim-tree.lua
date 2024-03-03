d4f74311dc9f6edadb04c33512c6dc94b029de31
monjara <yaginumee@gmail.com>
2024-03-03 19:13:54 +0900

    prepare nvim-tree

---
+++ nvim/lua/libs/nvim-tree.lua
@@ -0,0 +1,31 @@
+return {
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  lazy = false,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    vim.opt.termguicolors = true
+
+    require('nvim-tree').setup {
+      sort = {
+        sorter = 'case_sensitive',
+      },
+      view = {
+        width = 50,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = true,
+      },
+
+    }
+
+    vim.keymap.set('n', '<space>df', '<cmd>NvimTreeToggle<cr>', {})
+  end,
+}
