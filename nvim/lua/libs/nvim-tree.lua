6f5223d388f2f7d7e1682eb51034c9b8f650281e
monjara <yaginumee@gmail.com>
2024-03-17 14:35:08 +0900

    setting

--- nvim/lua/libs/nvim-tree.lua
+++ nvim/lua/libs/nvim-tree.lua
@@ -44,12 +44,26 @@ return {
       view = {
         float = {
           enable = true,
-          open_win_config = {
-            width = 80,
-            height = 30,
-            row = 1,
-            col = 20,
-          },
+          open_win_config = function()
+            local WIDTH_RATIO = 0.8
+            local HEIGHT_RATIO = 0.8
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * WIDTH_RATIO
+            local window_h = screen_h * HEIGHT_RATIO
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+            return {
+              border = 'rounded',
+              relative = 'editor',
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end
         }
       },
       renderer = {
