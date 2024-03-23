f116aaf537868b8facaa3cf37caa86997295784c
monjara <yaginumee@gmail.com>
2024-03-23 18:49:07 +0900

    add snippets and fix color

--- nvim/lua/libs/tokyonight.lua
+++ nvim/lua/libs/tokyonight.lua
@@ -3,8 +3,12 @@ return {
   config = function()
     require('tokyonight').setup {
       style = 'night',
-      light_style = 'night'
+      light_style = 'night',
+      on_colors = function(colors)
+        colors.bg = '#000000'
+      end
     }
+
     vim.cmd [[colorscheme tokyonight]]
   end
 }
