d9d2b94bf2f97ae02aaf192fb7072accc2b33273
monjara <yaginumee@gmail.com>
2024-03-31 14:23:43 +0900

    migrate rust-tools to rustaceanvim

--- nvim/lua/libs/nvim-lspconfig.lua
+++ nvim/lua/libs/nvim-lspconfig.lua
@@ -1,73 +1,71 @@
 return {
-  'neovim/nvim-lspconfig',
-  lazy = true,
-  event = { 'BufReadPost', 'BufAdd', 'BufNewFile' },
-  dependencies = {
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
+  {
+    'pmizio/typescript-tools.nvim',
+    ft = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
+    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    opt = {},
   },
-  config = function()
-    local lspconfig = require('lspconfig')
+  {
+    'neovim/nvim-lspconfig',
+    lazy = true,
+    event = { 'BufReadPost', 'BufAdd', 'BufNewFile' },
+    dependencies = {
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+    },
+    config = function()
+      local lspconfig = require('lspconfig')
 
-    local root_pattern = function(filename, pattern)
-      return lspconfig.util.root_pattern(pattern)(filename)
-    end
+      local root_pattern = function(filename, pattern)
+        return lspconfig.util.root_pattern(pattern)(filename)
+      end
 
-    -- lua
-    lspconfig.lua_ls.setup {
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { 'vim' },
-          },
-          format = {
-            enable = false,
-          },
-        },
-      },
-    }
-
-    -- rust
-    local rt = require('rust-tools')
-    rt.setup {
-      server = {
-        on_attach = function(_, bufnr)
-          vim.keymap.set('n', '<C-space>', rt.hover_actions.hover_actions, { buffer = bufnr })
-          vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
-        end,
+      -- lua
+      lspconfig.lua_ls.setup {
         settings = {
-          ['rust_analyzer'] = {
-            checkOnSave = {
-              command = 'clippy',
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' },
+            },
+            format = {
+              enable = false,
             },
           },
         },
-      },
-    }
+      }
 
-    -- swift
-    lspconfig.sourcekit.setup {
-      ft = { 'swift' },
-      cmd = {
-        '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp',
-      },
-      on_attach = function()
-        vim.api.nvim_create_user_command('SwiftFormat', '!swift format ' .. vim.api.nvim_buf_get_name(0) .. ' -i', {})
-      end,
-      root_dir = function(filename, _)
-        return root_pattern(filename, 'Package.swift')
-          or root_pattern(filename, 'buildServer.json')
-          or root_pattern(filename, '*.xcodeproj')
-          or root_pattern(filename, '*.xcworkspace')
-          or lspconfig.util.find_git_ancestor(filename)
-      end,
-    }
+      -- typescript, typescriptreact, javascript, javascriptreact
+      require('typescript-tools').setup {
+        settings = {
+          -- tsserver_locale = 'ja',
+        },
+      }
 
-    -- ruby
-    lspconfig.solargraph.setup {
-      ft = { 'ruby' },
-    }
+      -- swift
+      lspconfig.sourcekit.setup {
+        ft = { 'swift' },
+        cmd = {
+          '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp',
+        },
+        on_attach = function()
+          vim.api.nvim_create_user_command('SwiftFormat', '!swift format ' .. vim.api.nvim_buf_get_name(0) .. ' -i', {})
+        end,
+        root_dir = function(filename, _)
+          return root_pattern(filename, 'Package.swift')
+            or root_pattern(filename, 'buildServer.json')
+            or root_pattern(filename, '*.xcodeproj')
+            or root_pattern(filename, '*.xcworkspace')
+            or lspconfig.util.find_git_ancestor(filename)
+        end,
+      }
 
-    require('libs.nvim-lspconfig.init')
-  end,
+      -- ruby
+      lspconfig.solargraph.setup {
+        ft = { 'ruby' },
+      }
+
+      -- default
+      require('libs.nvim-lspconfig.init')
+    end,
+  },
 }
--- nvim/lua/libs/rust-tools_nvim.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'simrat39/rust-tools.nvim',
-  lazy = true,
-  ft = { 'rust', 'toml' },
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-}
