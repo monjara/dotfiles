c5a3a046797106e7fce5eef66a6258ea2f1d0d58
monjara <yaginumee@gmail.com>
2024-07-14 14:33:01 +0900

    ale and setting for CloudFormation

---
+++ nvim/lua/libs/ale.lua
@@ -0,0 +1,12 @@
+return {
+  'dense-analysis/ale',
+  config = function()
+    local g = vim.g
+
+    g.ale_ruby_rubocop_auto_correct_all = 1
+    g.ale_linters = {
+      lua = { 'lua_language_server' },
+      cloudformation = { 'cfn-lint' },
+    }
+  end,
+}
