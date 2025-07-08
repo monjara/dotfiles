867a148aa50fbc044bfb8d4cf4f17fad8802a260
monjara <yaginumtt@gmail.com>
2025-07-08 22:03:48 +0900

    fix setting

--- data/.config/nvim/lua/libs/hop_nvim.lua
+++ data/.config/nvim/lua/libs/hop_nvim.lua
@@ -3,10 +3,13 @@ return {
   version = '*',
   lazy = true,
   keys = {
-    { 't', '<cmd>HopWord<cr>', desc = 'hop', mode = { 'n', 'v' } },
+    { 't', '<cmd>HopWord<cr>',    desc = 'hop', mode = { 'n', 'v' } },
     { 's', '<cmd>HopChar1MW<cr>', desc = 'hop', mode = { 'n', 'v' } },
   },
   config = function()
-    require('hop').setup { keys = 'jfkdurghalsieownvmcxypq' }
+    require('hop').setup {
+      keys = 'jfkdurghalsieownvmcxypq',
+      case_insensitive = true,
+    }
   end,
 }
