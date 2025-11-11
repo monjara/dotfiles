6ea5fa1c4137d71b8ef1ceabc48350999cc7cc90
monjara <yaginumtt@gmail.com>
2025-11-11 23:31:23 +0900

    cmp

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -2,11 +2,30 @@ require('blink.cmp').setup {
   keymap = {
     preset = 'none',
     ['<cr>'] = { 'select_and_accept', 'fallback' },
-    ['<C-l>'] = { 'select_and_accept', 'fallback' },
-    ['<C-j>'] = { 'select_next', 'fallback' },
-    ['<C-k>'] = { 'select_prev', 'fallback' },
-    ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
-    ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
-    ['<C-h>'] = { 'show', 'show_documentation', 'hide_documentation' },
+    ['<c-l>'] = { 'select_and_accept', 'fallback' },
+    ['<c-j>'] = { 'select_next', 'fallback' },
+    ['<c-k>'] = { 'select_prev', 'fallback' },
+    ['<c-u>'] = { 'scroll_documentation_up', 'fallback' },
+    ['<c-d>'] = { 'scroll_documentation_down', 'fallback' },
+    ['<c-h>'] = { 'show', 'show_documentation', 'hide_documentation' },
   },
+  cmdline = {
+    completion = {
+      menu = {
+        auto_show = true
+      }
+    },
+    keymap = {
+      preset = 'none',
+      ['<c-l>'] = { 'select_and_accept', 'fallback' },
+      ['<c-j>'] = { 'select_next', 'fallback' },
+      ['<c-k>'] = { 'select_prev', 'fallback' },
+      ['<c-u>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<c-d>'] = { 'scroll_documentation_down', 'fallback' },
+      ['<c-h>'] = { 'show', 'show_documentation', 'hide_documentation' },
+    }
+  },
+  fuzzy = {
+    implementation = "rust"
+  }
 }
